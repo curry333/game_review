0659c5f7ed0fb045b49a111a715f2733b1c30abf
curry333 <aiueoh123@gmail.com>
2019-11-02 01:02:11 +0000

    ato kensaku

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,8 @@
 class ToppagesController < ApplicationController
   def index
     @reviews = Review.order(created_at: :desc).limit(5)
+    
+    reviews_averages = Review.group(:game_id).average(:score)
+    @ranks = Hash[reviews_averages.sort_by{ |k, v| -v }]
   end
 end
